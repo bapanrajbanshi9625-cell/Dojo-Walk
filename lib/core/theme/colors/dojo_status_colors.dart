8a3c22b71b8687bf8df1b9cecd6f3cb6d9528012
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:14:37 +0530

    Create dojo_status_colors.dart

---
+++ lib/core/theme/colors/dojo_status_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DojoStatusColors {
+  DojoStatusColors._();
+
+  // SUCCESS
+  static const Color success = Color(0xFF45C98A);
+  static const Color successSoft = Color(0xFFE2F8ED);
+  static const Color successDark = Color(0xFF18372D);
+
+  // WARNING
+  static const Color warning = Color(0xFFD9AD55);
+  static const Color warningSoft = Color(0xFFFFF4DB);
+  static const Color warningDark = Color(0xFF392F1C);
+
+  // ERROR / SOS
+  static const Color error = Color(0xFFE76565);
+  static const Color errorSoft = Color(0xFFFFE8E8);
+  static const Color errorDark = Color(0xFF351F21);
+
+  // INFO
+  static const Color info = Color(0xFF5CA9D6);
+  static const Color infoSoft = Color(0xFFE6F4FC);
+  static const Color infoDark = Color(0xFF1D303A);
+}
