2ea46c2348e7b7834c52c6199108e91c88b6894a
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:11:34 +0530

    Add DojoCardColors class for theme colors

---
+++ lib/core/theme/colors/dojo_card_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DojoCardColors {
+  DojoCardColors._();
+
+  // LIGHT
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightSoft = Color(0xFFF0F8F6);
+  static const Color lightBorder = Color(0xFFE0EBE9);
+  static const Color lightElevated = Color(0xFFFFFFFF);
+
+  // DARK
+  static const Color darkSurface = Color(0xFF122326);
+  static const Color darkElevated = Color(0xFF193237);
+  static const Color darkBorder = Color(0xFF294347);
+  static const Color darkStrongBorder = Color(0xFF35565A);
+
+  // Special
+  static const Color liveGlow = Color(0x4058E0D0);
+}
