76dffc4728908fdb883a4398e12d6d302f8cef86
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:12:09 +0530

    Add DojoButtonColors class for button color themes

---
+++ lib/core/theme/colors/dojo_button_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DojoButtonColors {
+  DojoButtonColors._();
+
+  // PRIMARY
+  static const Color primaryStart = Color(0xFF63EBDC);
+  static const Color primaryEnd = Color(0xFF58E0D0);
+  static const Color primaryText = Color(0xFF1C3136);
+
+  // LIGHT SECONDARY
+  static const Color lightSecondary = Color(0xFFECFAF8);
+  static const Color lightSecondaryText = Color(0xFF1C3136);
+  static const Color lightBorder = Color(0xFFE0EBE9);
+
+  // DARK SECONDARY
+  static const Color darkSecondary = Color(0xFF193237);
+  static const Color darkSecondaryText = Color(0xFF63EBDC);
+  static const Color darkBorder = Color(0xFF35565A);
+
+  // DANGER
+  static const Color danger = Color(0xFFE76565);
+  static const Color dangerText = Color(0xFFFFFFFF);
+
+  // GLOW
+  static const Color glow = Color(0x5958E0D0);
+}
