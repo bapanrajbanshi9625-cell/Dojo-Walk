e43cc963f0513842de17d8800ad5780a07a21237
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:15:16 +0530

    Add DojoOverlayColors class for color definitions

---
+++ lib/core/theme/colors/dojo_overlay_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DojoOverlayColors {
+  DojoOverlayColors._();
+
+  static const Color darkOverlay = Color(0x66000000);
+  static const Color lightOverlay = Color(0x22000000);
+
+  static const Color darkScrim = Color(0x99000000);
+  static const Color lightScrim = Color(0x33000000);
+
+  static const Color glow30 = Color(0x4D58E0D0);
+  static const Color glow40 = Color(0x6658E0D0);
+}
