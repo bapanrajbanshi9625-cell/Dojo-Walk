571215b285d1f94b4a20411a692e80c106f28ba0
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:11:03 +0530

    Create dojo_tab_colors.dart

---
+++ lib/core/theme/colors/dojo_tab_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DojoTabColors {
+  DojoTabColors._();
+
+  // LIGHT
+  static const Color lightBackground = Color(0xFFF0F8F6);
+  static const Color lightActive = Color(0xFF1C4248);
+  static const Color lightActiveText = Color(0xFFFFFFFF);
+  static const Color lightInactiveText = Color(0xFF667B7D);
+  static const Color lightIndicator = Color(0xFF63EBDC);
+
+  // DARK
+  static const Color darkBackground = Color(0xFF102023);
+  static const Color darkActive = Color(0xFF63EBDC);
+  static const Color darkActiveText = Color(0xFF1C3136);
+  static const Color darkInactiveText = Color(0xFFA5B9B9);
+  static const Color darkIndicator = Color(0xFF58E0D0);
+}
