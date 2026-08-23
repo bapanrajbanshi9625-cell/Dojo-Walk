7903f7cf4e7604b8956bee93d46a48b9ff9e4786
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:10:05 +0530

    Add DojoAppBarColors class for theme colors

---
+++ lib/core/theme/colors/dojo_appbar_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DojoAppBarColors {
+  DojoAppBarColors._();
+
+  // LIGHT
+  static const Color lightBackground = Color(0xFFF5F8F7);
+  static const Color lightTitle = Color(0xFF1C3136);
+  static const Color lightSubtitle = Color(0xFF667B7D);
+  static const Color lightIcon = Color(0xFF1C4248);
+  static const Color lightActiveIcon = Color(0xFF63EBDC);
+  static const Color lightDivider = Color(0xFFE0EBE9);
+
+  // DARK
+  static const Color darkBackground = Color(0xFF0B1719);
+  static const Color darkTitle = Color(0xFFF2FAF8);
+  static const Color darkSubtitle = Color(0xFFA5B9B9);
+  static const Color darkIcon = Color(0xFFA5B9B9);
+  static const Color darkActiveIcon = Color(0xFF63EBDC);
+  static const Color darkDivider = Color(0xFF294347);
+}
