0e4a45b73f68131646c4c00cb0a6b13dcba2891c
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-23 21:10:37 +0530

    Add DojoBottomColors class for theme colors

---
+++ lib/core/theme/colors/dojo_bottom_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DojoBottomColors {
+  DojoBottomColors._();
+
+  // LIGHT
+  static const Color lightBackground = Color(0xFFFFFFFF);
+  static const Color lightSelected = Color(0xFF1C3136);
+  static const Color lightSelectedText = Color(0xFF1C3136);
+  static const Color lightIndicator = Color(0xFFECFAF8);
+  static const Color lightUnselected = Color(0xFF91A2A3);
+  static const Color lightBorder = Color(0xFFE0EBE9);
+
+  // DARK
+  static const Color darkBackground = Color(0xFF122326);
+  static const Color darkSelected = Color(0xFF63EBDC);
+  static const Color darkSelectedText = Color(0xFF63EBDC);
+  static const Color darkIndicator = Color(0xFF24504F);
+  static const Color darkUnselected = Color(0xFF718889);
+  static const Color darkBorder = Color(0xFF294347);
+}
