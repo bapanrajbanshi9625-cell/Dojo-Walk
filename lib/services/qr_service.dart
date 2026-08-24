05caf391ed11437317367ae42bfe90d60d90ab62
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-24 23:13:49 +0530

    Update qr_service.dart

--- lib/services/qr_service.dart
+++ lib/services/qr_service.dart
@@ -2,8 +2,6 @@
 
 import 'dart:convert';
 
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
