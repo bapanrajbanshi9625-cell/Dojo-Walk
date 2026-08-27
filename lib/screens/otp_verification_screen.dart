006d0a14038aae30b4ba6ab2468786a9d87ebcab
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-27 15:21:55 +0530

    Update otp_verification_screen.dart

--- lib/screens/otp_verification_screen.dart
+++ lib/screens/otp_verification_screen.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import '../core/theme/dojotheme.dart';
+import '../core/constants/app_colors.dart';
 import '../services/owner_id_service.dart';
 import 'main_navigation_screen.dart';
 import 'profile_setup.dart';
