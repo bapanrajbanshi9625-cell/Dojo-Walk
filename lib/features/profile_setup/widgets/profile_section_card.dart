10ffda0a7948c4ef1284cd2709ddf9f4490bced8
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-28 03:18:21 +0530

    Update profile_section_card.dart

--- lib/features/profile_setup/widgets/profile_section_card.dart
+++ lib/features/profile_setup/widgets/profile_section_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/theme/colors/dojo_card_colors.dart';
+
 class ProfileSectionCard extends StatelessWidget {
   final Widget child;
 
@@ -14,13 +16,11 @@ class ProfileSectionCard extends StatelessWidget {
       width: double.infinity,
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius:
-            BorderRadius.circular(20),
+        color: DojoCardColors.lightBackground,
+        borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color:
-                Colors.black.withOpacity(0.05),
+            color: Colors.black.withOpacity(0.05),
             blurRadius: 12,
             offset: const Offset(0, 3),
           ),
