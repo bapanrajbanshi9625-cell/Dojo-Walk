82d61a7e6a7c4c747310e0538585ec14a863738b
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-08-28 20:00:05 +0530

    Update otp_verification_screen.dart

--- lib/screens/otp_verification_screen.dart
+++ lib/screens/otp_verification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sendotp_flutter_sdk/sendotp_flutter_sdk.dart';
 
 import '../core/constants/app_colors.dart';
