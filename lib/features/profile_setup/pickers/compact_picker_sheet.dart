69fb215aa0c88eb7de126c8621f4da5a24fecbeb
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-05 13:39:33 +0530

    Remove unused import in compact_picker_sheet.dart
    Removed unused import for app_colors.dart.

--- lib/features/profile_setup/pickers/compact_picker_sheet.dart
+++ lib/features/profile_setup/pickers/compact_picker_sheet.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/constants/app_colors.dart';
-
 class CompactPickerSheet extends StatelessWidget {
   final String title;
   final IconData icon;
