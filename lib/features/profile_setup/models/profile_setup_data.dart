51901819c6a215908acbac986be5bbc2e3ebd126
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-05 13:45:17 +0530

    Create profile_setup_data.dart

---
+++ lib/features/profile_setup/models/profile_setup_data.dart
@@ -0,0 +1,51 @@
+class ProfileSetupData {
+  static const List<String> ages = [
+    'Under 1 year',
+    '1 year',
+    '2 years',
+    '3 years',
+    '4 years',
+    '5 years',
+    '6 years',
+    '7 years',
+    '8 years',
+    '9 years',
+    '10+ years',
+  ];
+
+  static const List<String> breeds = [
+    'Labrador Retriever',
+    'Golden Retriever',
+    'German Shepherd',
+    'Beagle',
+    'Pug',
+    'Rottweiler',
+    'Shih Tzu',
+    'Siberian Husky',
+    'Doberman',
+    'Boxer',
+    'Great Dane',
+    'Dachshund',
+    'Cocker Spaniel',
+    'Border Collie',
+    'Pomeranian',
+    'Chihuahua',
+    'French Bulldog',
+    'English Bulldog',
+    'Indian Pariah',
+    'Indie',
+    'Rajapalayam',
+    'Mudhol Hound',
+    'Kombai',
+    'Chippiparai',
+    'Other',
+  ];
+
+  static const List<String> behaviours = [
+    'Friendly',
+    'Playful',
+    'Calm',
+    'Shy',
+    'Aggressive',
+  ];
+}
