33c79f63a92010dc6aac8a592ef5a25ec47de608
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-06 01:23:30 +0530

    Update live_walk_screen.dart

--- lib/features/live_walk/screens/live_walk_screen.dart
+++ lib/features/live_walk/screens/live_walk_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../models/live_walk_session.dart';
