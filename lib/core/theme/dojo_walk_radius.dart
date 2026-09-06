5166387926b729fc0e133774361cad422dc38927
bapanrajbanshi9625-cell <bapanrajbanshi9625@gmail.com>
2026-09-07 00:49:31 +0530

    Add DojoWalkRadius class with radius constants

---
+++ lib/core/theme/dojo_walk_radius.dart
@@ -0,0 +1,10 @@
+class DojoWalkRadius {
+  DojoWalkRadius._();
+
+  static const double sm = 8.0;
+  static const double md = 12.0;
+  static const double lg = 16.0;
+  static const double xl = 20.0;
+  static const double xxl = 24.0;
+  static const double pill = 999.0;
+}
